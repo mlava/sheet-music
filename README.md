9e0c528392675003a762f0c96706925029cc6ced
Mark Lavercombe <mlava@users.noreply.github.com>
2024-06-20 15:28:07 +1000

    update

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Create your own music using [abc notation](https://abcnotation.com/wiki/abc:stan
 
 <img width="1164" alt="image" src="https://github.com/mlava/sheet-music/assets/6857790/b6dda5bd-8722-4ee8-abfb-07a735a8275a">
 
-
 There are two options available at present:
 1. Paste music in the abc notation directly into your graph using the command palette option 'Paste sheet music in ABC notation'
 2. Create music in abc notation within your graph. Start with a blank template by using the command palette option 'Create sheet music in ABC notation'
@@ -19,3 +18,7 @@ Changes made in the abc notation child block will automatically render to the sh
 ![sheet-music](https://github.com/mlava/sheet-music/assets/6857790/9a360d0a-c9c0-4ef7-bd23-deab636e0a56)
 
 Standard abc notation applies - you can find a key to the notation at [abc notation](https://abcnotation.com/wiki/abc:standard).
+
+**Notes:**
+- In this first iteration, only one piece of music can be rendered on a page. I am working on a fix to allow multiple pieces to render correctly.
+- I am aware of an issue with overflow of the rendered sheet music on page resize, and am working on a fix.
--- src/sheetMusicComponent.jsx
+++ src/sheetMusicComponent.jsx
@@ -6,6 +6,7 @@ const SheetMusicElement = ({ blockUid }) => {
     const [abc, setAbc] = useState(0);
     var abc1 = "";
 
+    // get sheet music notation for rendering
     let blockData = window.roamAlphaAPI.data.pull("[:block/string :block/uid {:block/children ...}]", `[:block/uid \"${blockUid}\"]`);
     if (blockData.hasOwnProperty([":block/children"])) {
         abc1 += blockData[":block/children"][0][":block/string"];
@@ -13,17 +14,34 @@ const SheetMusicElement = ({ blockUid }) => {
     }
 
     var uid = blockData[":block/children"][0][":block/uid"];
-    function pullFunction(before, after) {
+    function pullFunction(before, after) { // set state to cause re-render on pullWatch change in abc notation block
         setAbc((keyValue => keyValue + 1));
     }
+
     useEffect(() => {
+        // add a pullWatch to update render if notation is changed
         window.roamAlphaAPI.data.addPullWatch(
             "[:block/string]",
             `[:block/uid "${uid}"]`,
             pullFunction);
 
+        // trying to fix display overflow on different screen sizes
+        /*
+        var svgs = document.getElementsByTagName("svg");
+        for (var i = 0; i < svgs.length; i++) {
+            if (svgs[i].innerHTML.startsWith("<title>Sheet Music</title>")) {
+                let width = Math.round(svgs[i].getAttribute("width"));
+                let height = Math.round(svgs[i].getAttribute("height"));
+                svgs[i].setAttribute("preserveAspectRatio", "none");
+                svgs[i].removeAttribute("width");
+                svgs[i].removeAttribute("height");
+                svgs[i].setAttribute("viewbox", `0 0 ${width} ${height}`);
+            }
+        }
+        */
+
         return () => {
-            window.roamAlphaAPI.data.removePullWatch(
+            window.roamAlphaAPI.data.removePullWatch( // remove pullWatch on remove
                 "[:block/string]",
                 `[:block/uid "${uid}"]`,
                 pullFunction);
